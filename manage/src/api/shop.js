f4745df5ac2b3bf0a1c1114e15c96896346a5b56
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-06-19 03:11:03 +0000

    12.1 api接口添加删除、创建、更新
    Signed-off-by: starry <9738097+boundless-starry-sky@user.noreply.gitee.com>

--- manage/src/api/shop.js
+++ manage/src/api/shop.js
@@ -1,6 +1,7 @@
 import instance from '@/utils/axios-accept';
 
 export default {
+  // 获取列表
   getList: (params) => {
     return instance({
       url: '/api/shop-list',
@@ -8,10 +9,27 @@ export default {
       params,
     });
   },
+  // 删除数据
   delete: (id) => {
     return instance({
       url: `/api/shop-list/${id}/delete`,
       method: 'DELETE',
+    });
+  },
+  // 新建列表
+  post: (data) => {
+    return instance({
+      url: '/api/shop-list/add',
+      method: 'POST',
+      data,
+    });
+  },
+  // 更新数据
+  update: (data) => {
+    return instance({
+      url: '/api/shop-list/update',
+      method: 'POST',
+      data,
     });
   },
 };
