84bf9bb3afef33031ee9cccf9bc14d09917e6346
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-06-20 03:00:51 +0000

    14.0 getClassify函数独立
    Signed-off-by: starry <9738097+boundless-starry-sky@user.noreply.gitee.com>

---
+++ serve/utils/getClassify.js
@@ -0,0 +1,18 @@
+// 处理classify数据类型，叠加
+const getClassify = (type = 'productName') => {
+    return baseList.reduce((total, item) => {
+        return {
+        ...total,
+        [item?.[type]]: {
+            // 存在数量
+            totalNumber: (total?.[type]?.totalNumber || 0) + (item?.total || 0),
+            // 剩余数量
+            resetNumber: (total?.[type]?.resetNumber || 0) + (item?.number || 0),
+            // 所有产品
+            product: [...(item?.[type]?.product || []), item?.productName],
+        },
+        };
+    }, {});
+};
+
+module.exports = getClassify;
