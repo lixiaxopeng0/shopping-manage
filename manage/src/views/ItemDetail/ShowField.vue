c899400fa12ef30496a5583b537ee0e4175b6fd3
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-06-21 03:17:36 +0000

    16.2 详情展示字段组件添加
    Signed-off-by: starry <9738097+boundless-starry-sky@user.noreply.gitee.com>

---
+++ manage/src/views/ItemDetail/ShowField.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="filed" :style="{width: labelWidth ?? 'auto'}">
+        <p class="label">{{ label }}:</p>
+        <div class="field-value"><slot></slot></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ShowField',
+    props: ['label', 'labelWidth']
+}
+</script>
+<style lang="less" scoped>
+.filed {
+    display: flex;
+    margin-bottom: 20px;
+
+    .label {
+        padding-right: 5px;
+        min-width: 80px;
+        text-align: start;
+    }
+
+    .field-value {
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
