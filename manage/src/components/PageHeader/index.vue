84c729c30cb5ba6a1b8c248fdf396e9838265dba
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-06-21 06:44:47 +0000

    16.4 共用表头添加
    Signed-off-by: starry <9738097+boundless-starry-sky@user.noreply.gitee.com>

---
+++ manage/src/components/PageHeader/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-row>
+        <el-col :span="24">
+            <div class="page-header">
+                <el-button icon="el-icon-arrow-left" class="back-button" size="mini" @click="click"></el-button>
+                <slot></slot>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+<script>
+export default {
+    name: 'PageHeader',
+    methods: {
+        click() {
+            this.$emit('onBack');
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.page-header {
+    padding: 0 10px;
+    display: flex;
+    height: 50px;
+    border-bottom: 1px solid #f4f2f2;
+    box-shadow: 3px 3px 10px 0px #b7b6b6;
+    margin-bottom: 10px;
+    align-items: center;
+}
+
+.back-button::v-deep {
+    margin-right: 10px;
+}
+</style>
